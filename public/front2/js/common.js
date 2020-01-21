f25568eac4026ae6f207c74eafdbe1abd0b2141e
wlwp <wl15021520711@163.com>
2020-01-21 22:21:00 +0800

    首页完成

--- public/front2/js/common.js
+++ public/front2/js/common.js
@@ -1,7 +1,13 @@
 $(function() {
-  // 区域滚动
+  // 区域滚动效果
   mui('.mui-scroll-wrapper').scroll({
     deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
     indicators: false //是否显示滚动条
+  })
+
+  // 轮播图效果,获得slider插件对象
+  var gallery = mui('.mui-slider')
+  gallery.slider({
+    interval: 3000 //自动轮播周期，若为0则不自动播放，默认为0；
   })
 })
